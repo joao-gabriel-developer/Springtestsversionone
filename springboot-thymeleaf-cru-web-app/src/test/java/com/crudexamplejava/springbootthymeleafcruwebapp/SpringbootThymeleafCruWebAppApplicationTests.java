1f88a9a7696834aea291855e10e0498c71ae0f6f
joao-gabriel-developer <joaogabrieloprogramador@gmail.com>
2022-07-26 18:27:58 -0300

    create repository
    create repository

---
+++ springboot-thymeleaf-cru-web-app/src/test/java/com/crudexamplejava/springbootthymeleafcruwebapp/SpringbootThymeleafCruWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudexamplejava.springbootthymeleafcruwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafCruWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
