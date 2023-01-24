77d57b537fd91b141d487d61b4f81474a3b58eec
joao-gabriel-developer <joaogabrieloprogramador@gmail.com>
2023-01-24 11:52:23 -0300

    create class user

--- MyWebApp/src/main/java/com/mycompany/mywebapp/MainController.java
+++ MyWebApp/src/main/java/com/mycompany/mywebapp/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("")
     public String showHomePage() {
-        System.out.println("main controller");
+
         return"index";
     }
 }
